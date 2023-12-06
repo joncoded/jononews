0e647c065a215c4022ce6a5cfaca3c385cfeda02
Joncoded <joncoded@gmail.com>
2023-12-05 22:52:52 -0500

    updating homepage file

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from "react"
 import { MainDiv } from "../components/main"
 import Link from "next/link"
 
@@ -27,7 +28,7 @@ export default async function Main({searchParams}: MainProps) {
   const { query, page } = searchParams    
   const data = await getData(query, page)  
 
-  const getURLHost = (url) => {
+  const getURLHost = (url: string) => {
     return (url) ? new URL(url).host.toString() : ''
   }
 
@@ -38,16 +39,16 @@ export default async function Main({searchParams}: MainProps) {
   return (
     <>
 
-      <nav className="sticky z-40 p-5 bg-green-500 w-full shadow-xl">
-        <div className="max-w-screen-xl mx-auto">
-          <div className="flex justify-between items-center gap-5">
-            <h2 className="text-md md:text-5xl text-black font-bold uppercase">
+      <nav className="navi-wrap sticky top-0 z-40 p-5 bg-green-500 w-full shadow-xl">
+        <div className="navi-prop max-w-screen-xl mx-auto">
+          <div className="navi-flex flex justify-between items-center gap-5">
+            <h2 className="navi-name text-md md:text-5xl text-black font-bold uppercase">
               {queryLabel} 
               {page ? ` / ${page} ` : ''}
             </h2>
-            <div className="flex gap-5">
-              { newerPage && <Link href={`?query=${query}&page=${newerPage}`}>newer</Link>}
-              { olderPage && <Link href={`?query=${query}&page=${olderPage}`}>older</Link>}
+            <div className="navi-page flex gap-5">
+              { newerPage > 0 && <Link href={`?query=${query}&page=${newerPage}`}>newer</Link>}
+              { olderPage > 0 && <Link href={`?query=${query}&page=${olderPage}`}>older</Link>}
             </div>
           </div>
         </div>
@@ -56,23 +57,31 @@ export default async function Main({searchParams}: MainProps) {
       <MainDiv>  
         
         <div className="main-list grid grid-cols-1 gap-1">
-          {data.hits.map(hit =>                   
-
-            <article className="main-item mt-5" key={hit.objectID}>
-
-              <aside className="main-date text-gray-700 text-sm">
-                {`${hit.created_at.substring(0,10)} ${hit.created_at.substring(11,16)}`} 
-                {hit.url && <span className="main-host ml-1 text-gray-400">via {getURLHost(hit.url)}</span>}
-              </aside>
-                                              
-              <h3 className="main-link text-3xl lowercase">
-                <Link href={hit.url ?? ''} target="_blank">{hit.title}</Link>
-              </h3>            
+          {data.hits.map((hit: any) =>                   
+            
+            <Fragment key={hit.objectID}>
               
-            </article>            
+              {hit.url && 
+              
+                <article className="main-item mt-5">
+
+                  <aside className="main-date text-gray-700 text-sm">
+                    {`${hit.created_at.substring(0,10)} ${hit.created_at.substring(11,16)}`} 
+                    <span className="main-host ml-1 text-gray-400">via {getURLHost(hit.url)}</span>
+                  </aside>
+                                                  
+                  <h3 className="main-link text-3xl lowercase">
+                    <Link href={hit.url} target="_blank">{hit.title}</Link>
+                  </h3>            
+                  
+                </article>
+            
+              }
+
+            </Fragment>
 
           )}
-
+          
           {data.hits.length === 0 && (
 
             <p>no items or end of file</p>
