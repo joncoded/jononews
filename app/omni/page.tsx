3a50e8ea224f2add7f058a94f9cdc337dde781e4
Joncoded <joncoded@gmail.com>
2023-12-07 10:48:20 -0500

    proposing omni platform

---
+++ app/omni/page.tsx
@@ -0,0 +1,58 @@
+import { Fragment } from "react"
+import { PageNavi, NaviName, NaviPage } from "../../components/navi"
+import { MainDiv, MainList } from "../../components/main"
+import Item from "../../components/item"
+import { text } from "../../components/text"
+import { getData } from "../../util/data"
+
+interface MainProps {
+  params: {
+    slug: string
+  },
+  searchParams: {    
+    page?: number,
+    points?: number
+  }
+}
+
+export default async function Main({params, searchParams}: MainProps) {
+
+  const { slug = '' } = params  
+  const { page = 1, points = 0 } = searchParams
+  const data = await getData(slug, page - 1, points)
+  const { hits: list } = data    
+  
+  return (
+    <>
+
+      <PageNavi>
+        <NaviName label={slug} page={page} />
+        <NaviPage platform="term" slug={slug} current={page} points={points} />
+      </PageNavi>    
+    
+      <MainDiv>  
+        
+        { list.length > 0 && 
+        
+          <MainList>
+            {list.map((item: any) =>              
+              <Fragment key={item.objectID}>                
+                {item.url && <Item item={item} /> }
+              </Fragment>
+            )}                      
+          </MainList>
+        
+        }
+
+        {list.length === 0 && (
+
+          <p>{text["no items"]}</p>
+
+        )}
+
+      </MainDiv>
+
+    </>
+  )
+
+}
