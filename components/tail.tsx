81dd3c18aac0add744bb827af663439b3d582655
Joncoded <joncoded@gmail.com>
2023-12-07 10:42:09 -0500

    updating tail thickness

--- components/tail.tsx
+++ components/tail.tsx
@@ -1,7 +1,7 @@
 export default function Tail () {
   return (
-    <footer className="p-5 bg-black text-white">
-      <div className="w-screen-xl max-w-screen-xl mx-auto">
+    <footer className="p-5 py-16 mt-10 bg-black">
+      <div className="w-screen-xl max-w-screen-xl mx-auto text-white text-center md:text-left">
         a @joncoded <span aria-hidden="true">🐼🧑🏻‍💻</span> project
       </div>
     </footer>
