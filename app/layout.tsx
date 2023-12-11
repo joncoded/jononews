70c5dfb90f7ff62e115ae28fa221e877cf5342cb
Joncoded <joncoded@gmail.com>
2023-12-11 18:09:25 -0500

    adding favicon file

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+      <link rel="shortcut icon" href={`/favicon.png?v=${Date.now()}`} />
+      </head>
       <body>
         <ThemeProvider attribute={`class`} defaultTheme={`light`} enableSystem>
           <Head />        
